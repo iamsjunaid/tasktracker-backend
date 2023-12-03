0225b845a06689d54073ed57a90c4ee950d925db
Junaid Syed <sjunaid626@gmail.com>
2023-12-03 16:56:52 +0530

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
- resources :tasks, only: :index
+
+  namespace :api do
+    namespace :v1 do
+      resources :tasks, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+
 end
