34de7056513f9494973b3b378943e03d43edbfed
Junaid Syed <sjunaid626@gmail.com>
2023-12-03 16:56:30 +0530

    Update application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found(e)
+    render json: { error: e.record.errors }, status: :unprocessable_entity
+  end
 end
