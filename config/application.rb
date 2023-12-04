bbcbf5f9989ad9c7b1628c41b4620d1ce49f186d
Junaid Syed <sjunaid626@gmail.com>
2023-12-04 16:36:20 +0530

    Fix error in production

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module TasktrackerBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.assets.enabled = true
   end
 end
